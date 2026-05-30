4a09523850cfeebdfa692b54279caaa0c7c1689b
DevilShekhar <shekhar@eternalhightech.com>
2026-05-30 12:49:04 +0530

    testing deployment

--- routes/api.php
+++ routes/api.php
@@ -5,8 +5,6 @@ use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\API\AuthController;
 use App\Http\Controllers\API\UserController;
 
-
-
 Route::post('register', [AuthController::class, 'register']);
 Route::post('login', [AuthController::class, 'login']);
 
