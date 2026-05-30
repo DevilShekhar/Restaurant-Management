23e5d3c548dc4f5f8dd9ba505b2b70f25b8c75d5
Abhishek-10hue <ehtabhishek@eternalhightech.com>
2026-05-30 18:24:16 +0530

    Some files to be added and blade view changes

--- app/Http/Controllers/Admin/AuthController.php
+++ app/Http/Controllers/Admin/AuthController.php
@@ -9,19 +9,19 @@ use Illuminate\Support\Facades\Auth;
 class AuthController extends Controller
 {
     //
-    public function webLogin(Request $request)
-    {
-        $credentials = $request->only('email', 'password');
+  public function webLogin(Request $request)
+{
+    $credentials = $request->only('email', 'password');
 
-        if (Auth::attempt($credentials)) {
+    if (Auth::attempt($credentials)) {
 
-            $request->session()->regenerate();
+        $request->session()->regenerate();
 
-            return redirect('/dashboard');
-        }
-
-        return back()->withErrors([
-            'email' => 'Invalid credentials.',
-        ]);
+        return redirect()->route('dashboard');
     }
+
+    return back()->withErrors([
+        'email' => 'Invalid credentials.',
+    ]);
+}
 }
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,7 @@
 use App\Http\Controllers\Admin\AuthController as AdminAuthController;
 use App\Http\Controllers\AuthController;
 use App\Http\Controllers\ProfileController;
+use Illuminate\Support\Facades\Auth;
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
@@ -11,14 +12,15 @@ Route::get('/', function () {
 // Route::post('login', [AuthController::class, 'login']);
 
 Route::get('/dashboard', function () {
+    
     return view('dashboard');
-})->middleware(['auth', 'verified'])->name('dashboard');
+})->name('dashboard')->middleware('auth:web');
 
-Route::middleware('auth')->group(function () {
+// Route::middleware('auth')->group(function () {
     Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
     Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
     Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
-});
+// });
 
 Route::get('/web-login', function () {
     return view('auth.login');
@@ -26,4 +28,5 @@ Route::get('/web-login', function () {
 
 Route::post('/web-login', [AdminAuthController::class, 'webLogin'])
 
-    ->name('web.login');require __DIR__.'/auth.php';
+    ->name('web.login');
+require __DIR__ . '/auth.php';
